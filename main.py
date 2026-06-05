1350e1ca76df3d62c22440564c5e9968e80b5445
Omar Reda <or60969@gmail.com>
2026-06-06 02:05:44 +0300

    fixed bug: load>FE>Split>transform(x_test)>eval

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from src.pipelines.training_pipeline import TrainingPipeline
+from src.models.predict import Test
 from src.utils.logger import setup_logger
 
 if __name__ == "__main__":
     setup_logger()
-    pipeline = TrainingPipeline()
-    pipeline.run_pipeline()
+    #pipeline = TrainingPipeline()
+    #pipeline.run_pipeline()
+    tester=Test()
+    tester.run_test()
+
--- src/models/predict.py
+++ src/models/predict.py
@@ -22,11 +22,12 @@ class Test:
         test_df=self.ingestor.load_data(file_name=filename,is_train=False)
         # 2. Feature Engineering
         test_df=self.engineer.add_features(test_df)
-        # 3. Preprocessing (Scaling/Imputing)
-        test_df=self.preprocessor.transform(test_df)
-        # 4. Split X and y
+        # 3. Split X and y
         y_test = test_df['Class']
         X_test = test_df.drop('Class', axis=1)
+        # 4. Preprocessing (Scaling/Imputing)
+        test_df=self.preprocessor.transform(X_test)
+        
 
         logging.info("--- Evaluating Model on Testing Data ---")
         self.evaluator.evaluate_model(self.model, X_test, y_test)
